c24fa6b37cbae0b1c868169e46a0bbd6693548fa
Remy Phan <phan.remy@gmail.com>
2019-11-25 18:32:02 +0100

    rules and game event start

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,13 @@
 class GamesController < ApplicationController
+  def create
+    @game = Game.new(game_params)
+    @game.save
+    redirect_to game_game_event_path(@game_event)
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:user_id)
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def rules
+    @game = Game.find(params[:game_id])
+    @game_event = GameEvent.new
+  end
 end
