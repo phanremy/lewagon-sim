95bb271e638fffcad38ca00699010b463fb24827
Benjamin Leray <b.leray35@gmail.com>
2019-11-25 16:50:22 +0100

    j'étais sur la route toute la sainte journée

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "rules", to: "pages#rules"
+  resources :games, only: [:show, :create, :update] do
+    resources :game_events, only: [:show, :create, :update]
+  end
 end
