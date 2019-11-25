0fc1944ba9ffaf15c395082bea2ac0db2618e3e7
Nicolas Pierson <nicolas.pierson57@gmail.com>
2019-11-25 18:42:24 +0100

    creation de la show game event

--- app/controllers/game_events_controller.rb
+++ app/controllers/game_events_controller.rb
@@ -1,2 +1,7 @@
 class GameEventsController < ApplicationController
+  def show
+    @game_event = GameEvent.find(params[:game_id])
+    @event = Event.find(@game_event.event_id)
+    @game = Game.find(params[:id])
+  end
 end
