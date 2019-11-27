40f02df65d138efa6d0c21cdaf4ee7e22d084918
Remy Phan <phan.remy@gmail.com>
2019-11-27 15:37:40 +0100

    start of home & background

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,27 @@
-import "bootstrap";
-import { initSortable } from '../plugins/init_sortable';
+// import "bootstrap";
+// import { initSortable } from '../plugins/init_sortable';
 
 
-function incrementScore() {
-  const new_score = document.querySelector("[data-score-display]");
-  setInterval(function(){
-    new_score.innerHTML = new_score.dataset.scoreDisplay;
+// function incrementScore() {
+//   const new_score = document.querySelector("[data-score-display]");
+//   setInterval(function(){
+//     new_score.innerHTML = new_score.dataset.scoreDisplay;
 
-  }, 50)
+//   }, 50)
+// };
+
+// incrementScore();
+// initSortable();
+
+
+import Typed from 'typed.js';
+
+var options = {
+  stringsElement: '#typed-strings',
+  typeSpeed: 40,
+  loop: true,
+  loopCount: Infinity,
 };
 
-incrementScore();
-initSortable();
 
+var typed = new Typed('#typed', options);
