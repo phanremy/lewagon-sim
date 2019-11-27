da6c2f60afa26304eb5a8d38443ff45435a25284
Remy Phan <phan.remy@gmail.com>
2019-11-27 15:40:46 +0100

    putain putain putain

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,20 +1,18 @@
-// import "bootstrap";
-// import { initSortable } from '../plugins/init_sortable';
-
-
-// function incrementScore() {
-//   const new_score = document.querySelector("[data-score-display]");
-//   setInterval(function(){
-//     new_score.innerHTML = new_score.dataset.scoreDisplay;
-
-//   }, 50)
-// };
-
-// incrementScore();
-// initSortable();
-
-
+import "bootstrap";
 import Typed from 'typed.js';
+import { initSortable } from '../plugins/init_sortable';
+
+
+function incrementScore() {
+  const new_score = document.querySelector("[data-score-display]");
+  setInterval(function(){
+    new_score.innerHTML = new_score.dataset.scoreDisplay;
+
+  }, 50)
+};
+
+incrementScore();
+initSortable();
 
 var options = {
   stringsElement: '#typed-strings',
