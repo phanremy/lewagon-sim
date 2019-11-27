301d5451d717ab99abb837ab724f07d8bc5ddfe1
Nicolas Pierson <nicolas.pierson57@gmail.com>
2019-11-27 10:42:18 +0100

    add realtime counter for score

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+
+function incrementScore() {
+  const new_score = document.querySelector("[data-score-display]");
+  setInterval(function(){
+    new_score.innerHTML = new_score.dataset.scoreDisplay;
+
+  }, 50)
+};
+
+incrementScore();
+
+
