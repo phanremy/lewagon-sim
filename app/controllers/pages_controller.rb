6895bd3ef273e64ce720f129e541bafbe30b35ea
Remy Phan <phan.remy@gmail.com>
2019-11-28 18:38:33 +0100

    3 methods of event sequence, default is random

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,7 +21,7 @@ class PagesController < ApplicationController
     # @game6 = Game.find(6)
     # @all_possible_events = Event.all
     # @all_events_of_game6 = @game6.game_events.map { |game_event| game_event.event.id }
-    # @possible_events_game6_gameevent2 = @all_possible_events.reject { |event| @all_events_of_game6.include?(event.id) }
+    # @possible_events_game6_gameevent2 = @all_possible_events.reject {|event| @all_events_of_game6.include?(event.id)}
     # @random_event = @possible_events_game6_gameevent2.sample
   end
 
