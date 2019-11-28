7813d3563b96ef0059d186d0f3bd0599e7f254b7
Benjamin Leray <b.leray35@gmail.com>
2019-11-28 13:28:59 +0100

    Added 3 events text without pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,160 +37,216 @@ game_1 = Game.new( score: 0, stress: 0, user_id: user_1)
 game_1.save
 
 #-------------------------------------------------------------
+puts "Creating first day event"
+first_day = Event.new({
+  title: "First Day",
+  description: " This is your first day at « Villa Gaudelet », you’re exited to begin , you just met 30 new buddies and you have to  install all the files needed for working easily. Where do you spend the most time?" ,
+  image_url: nil,
+  })
+first_day.save
+
+puts "Creating first day choices"
+ first_day_choice_1 = Choice.new({
+  title: "Discuss with schoolmates" ,
+  description: "Good Choice, you’ll spend 9 weeks with them, it’s important to know your next buddies.",
+  score_impact: 450 ,
+  stress_impact: 0,
+})
+
+first_day_choice_2 = Choice.new({
+  title: "follow the set-up instructions",
+  description:"Well… You must install Linux, witch takes a lot of time, and a lot of bug appears. All the teachers have to help you. You’re a bit embarrassed." ,
+  score_impact: 120,
+  stress_impact: 20,
+})
+
+ first_day_choice_1.event = first_day
+ first_day_choice_2.event = first_day
+ first_day_choice_1.save
+ first_day_choice_2.save
+puts "done"
+
+puts "Creating Air-BnB event"
+airbnb = Event.new({
+  title: "Air Bnb Project",
+  description: " You have to work a whole week with 3 Buddies, to make a copycat of Airbnb. You can choose any subject you want ! What do you want to rent?",
+  image_url: nil,
+  })
+airbnb.save
+
+puts "Creating Air-Bnb choices"
+ airbnb_choice_1 = Choice.new({
+  title: "Manors" ,
+  description: "Pff... Boring !!!",
+  score_impact: 175,
+  stress_impact: 10,
+})
+
+airbnb_choice_2 = Choice.new({
+  title: "Socks",
+  description:"Yes ! You have the good spirit. No need to be serious for the moment." ,
+  score_impact: 560,
+  stress_impact: 0,
+})
+
+ airbnb_choice_1.event = airbnb
+ airbnb_choice_2.event = airbnb
+ airbnb_choice_1.save
+ airbnb_choice_2.save
+puts "done"
+
+
 puts "Creating Yoga event"
 event_yoga = Event.new({
   title: "Yoga",
-  description: "Il est 16H déjà, tu viens de recevoir un message de la prof de yoga, qui annonce que son cours commence dans 15 Min. Que fais-tu?",
+  description: " This is the middle of the afternoon, you just receive a message from the Yoga teacher, it's begin in 15 minutes. What do you do?",
   image_url: nil,
   })
 
 event_yoga.save
 
-puts " Choice 1 Yoga"
+puts "Choices Yoga"
  event_yoga_choice_1 = Choice.new({
-  title: "Je vais au Yoga",
-  description: "cool",
-  score_impact: 10,
-  stress_impact: 0,
+  title: "Time to Yoga!",
+  description: " It was very efficient on your moral. At the end of the day you didn't finish your day-work, but you are way more efficent the next day",
+  score_impact: 320,
+  stress_impact: 5,
 })
 
  event_yoga_choice_1.event = event_yoga
  event_yoga_choice_1.save
 
-puts " Choice 2 Yoga"
 event_yoga_choice_2 = Choice.new({
-  title:"Je continue de travailler",
-  description:" Pas cool" ,
-  score_impact:  20,
-  stress_impact: 10 ,
+  title: "Keep working",
+  description: "You rush into your work, but you're tired, this hour of working is useless, you learned nothing. You are just more stressed. " ,
+  score_impact:  10,
+  stress_impact: 30 ,
 })
 event_yoga_choice_2.event = event_yoga
 event_yoga_choice_2.save
 
-#-------------------------------------------------------------
-puts "Creating Embuscade event"
-event_embuscade = Event.new({
-  title: "Embuscade",
-  description: "Embuscade! on te propose d'aller prendre une biere",
-  image_url: nil,
-  })
+puts "done"
 
-event_embuscade.save
+# #-------------------------------------------------------------
+# puts "Creating Embuscade event"
+# event_embuscade = Event.new({
+#   title: "Embuscade",
+#   description: "Embuscade! on te propose d'aller prendre une biere",
+#   image_url: nil,
+#   })
 
-puts " Choice 1 Embuscade"
- event_embuscade_choice_1 = Choice.new({
-  title: "J'accepte",
-  description: "pas cool",
-  score_impact: 10,
-  stress_impact: 20,
-})
+# event_embuscade.save
 
- event_embuscade_choice_1.event = event_embuscade
- event_embuscade_choice_1.save
+# puts " Choice 1 Embuscade"
+#  event_embuscade_choice_1 = Choice.new({
+#   title: "J'accepte",
+#   description: "pas cool",
+#   score_impact: 10,
+#   stress_impact: 20,
+# })
 
-puts " Choice 2 Embuscade"
-event_embuscade_choice_2 = Choice.new({
-  title:"Je rentre faire mes flashcards",
-  description:"cool" ,
-  score_impact:  20,
-  stress_impact: 00 ,
-})
-event_embuscade_choice_2.event = event_embuscade
-event_embuscade_choice_2.save
+#  event_embuscade_choice_1.event = event_embuscade
+#  event_embuscade_choice_1.save
 
-#-------------------------------------------------------------
-puts "Creating Push-Master event"
-event_push_master = Event.new({
-  title: "Git Push Origin Master",
-  description: "T’as git push origin master par erreur 3 jours avant le démo day, et t’étais pas à jour, t’as fais disparaitre une bonne partie du taf d’un de tes buddies",
-  image_url: nil,
-  })
+# puts " Choice 2 Embuscade"
+# event_embuscade_choice_2 = Choice.new({
+#   title:"Je rentre faire mes flashcards",
+#   description:"cool" ,
+#   score_impact:  20,
+#   stress_impact: 00 ,
+# })
+# event_embuscade_choice_2.event = event_embuscade
+# event_embuscade_choice_2.save
 
-event_push_master.save
+# #-------------------------------------------------------------
+# puts "Creating Push-Master event"
+# event_push_master = Event.new({
+#   title: "Git Push Origin Master",
+#   description: "T’as git push origin master par erreur 3 jours avant le démo day, et t’étais pas à jour, t’as fais disparaitre une bonne partie du taf d’un de tes buddies",
+#   image_url: nil,
+#   })
 
-puts " Choice 1 Push-Master"
- event_push_master_choice_1 = Choice.new({
-  title: "Tu joues la mauvaise foi.. Non non j’étais sur une branche, je vous assure..",
-  description: "T’es un crétin, ça leur a pris trois seconde pour vérifier tes dires sur GitHub et tu t’es fais griller.. stress ++, malus sur les prochaines questions, tes buddies on les nerfs",
-  score_impact: 0,
-  stress_impact: 50,
-})
+# event_push_master.save
 
- event_push_master_choice_1.event = event_push_master
- event_push_master_choice_1.save
+# puts " Choice 1 Push-Master"
+#  event_push_master_choice_1 = Choice.new({
+#   title: "Tu joues la mauvaise foi.. Non non j’étais sur une branche, je vous assure..",
+#   description: "T’es un crétin, ça leur a pris trois seconde pour vérifier tes dires sur GitHub et tu t’es fais griller.. stress ++, malus sur les prochaines questions, tes buddies on les nerfs",
+#   score_impact: 0,
+#   stress_impact: 50,
+# })
 
-puts " Choice 2 Push-Master"
-event_push_master_choice_2 = Choice.new({
-  title:"Tu tentes un ctrl-Z de derrière les fagots sur la page stt du crime ouverte devant toi sur le pc de ton buddy.",
-  description:"Maxi coup de chatte, le code disparu réapparait à l’écran ! Tout le monde s’accorde pour dire qu’on a frôlé la catastrophe mais qu’on s’est bien marré au final.. stress +, bonus pour les prochaines questions : You are on FIRE !" ,
-  score_impact:  50,
-  stress_impact: 00 ,
-})
-event_push_master_choice_2.event = event_push_master
-event_push_master_choice_2.save
+#  event_push_master_choice_1.event = event_push_master
+#  event_push_master_choice_1.save
 
-#-------------------------------------------------------------
-puts "Creating Flemme Cours event"
-event_flemme = Event.new({
-  title: "Flemme de noter en cours",
-  description: "T’as trop trainé hier soir et t’as une flemme monumentale ce matin, t’as vraiment aucune envie de prendre des notes..",
-  image_url: nil,
-  })
+# puts " Choice 2 Push-Master"
+# event_push_master_choice_2 = Choice.new({
+#   title:"Tu tentes un ctrl-Z de derrière les fagots sur la page stt du crime ouverte devant toi sur le pc de ton buddy.",
+#   description:"Maxi coup de chatte, le code disparu réapparait à l’écran ! Tout le monde s’accorde pour dire qu’on a frôlé la catastrophe mais qu’on s’est bien marré au final.. stress +, bonus pour les prochaines questions : You are on FIRE !" ,
+#   score_impact:  50,
+#   stress_impact: 00 ,
+# })
+# event_push_master_choice_2.event = event_push_master
+# event_push_master_choice_2.save
 
-event_flemme.save
+# #-------------------------------------------------------------
+# puts "Creating Flemme Cours event"
+# event_flemme = Event.new({
+#   title: "Flemme de noter en cours",
+#   description: "T’as trop trainé hier soir et t’as une flemme monumentale ce matin, t’as vraiment aucune envie de prendre des notes..",
+#   image_url: nil,
+#   })
 
-puts " Choice 1 Flemme Cours"
- event_flemme_choice_1 = Choice.new({
-  title: "Tu te forces à prendre des notes, ça me servira plus tard sans aucun doute.",
-  description: "T’as fourni un effort relatif (mais important dans ton état) et au final t’as copié la moitié de travers, faut que tu retournes dans les slides pour aller trouver les infos que tu cherches. Stress + t’es deg",
-  score_impact: 10,
-  stress_impact: 20,
-})
+# event_flemme.save
 
- event_flemme_choice_1.event = event_flemme
- event_flemme_choice_1.save
+# puts " Choice 1 Flemme Cours"
+#  event_flemme_choice_1 = Choice.new({
+#   title: "Tu te forces à prendre des notes, ça me servira plus tard sans aucun doute.",
+#   description: "T’as fourni un effort relatif (mais important dans ton état) et au final t’as copié la moitié de travers, faut que tu retournes dans les slides pour aller trouver les infos que tu cherches. Stress + t’es deg",
+#   score_impact: 10,
+#   stress_impact: 20,
+# })
 
-puts " Choice 2 Flemme Cours"
-event_flemme_choice_2 = Choice.new({
-  title:"La flemme l’emporte et tu décides de suivre nonchalamment le cours.",
-  description:"Pari gagnant ! Toutes les infos dont tu as besoin sont dans les slides et sources du cours. Stress -, malus tu prends des mauvaises habitudes ??",
-  score_impact:  20,
-  stress_impact: 00 ,
-})
-event_flemme_choice_2.event = event_flemme
-event_flemme_choice_2.save
+#  event_flemme_choice_1.event = event_flemme
+#  event_flemme_choice_1.save
 
-#-------------------------------------------------------------
-puts "Creating Scaffold event"
-event_scaffold = Event.new({
-  title: "Scaffold!",
-  description: "On nous a appris le scaffold, pour eviter de faire ces methodes interminables et repetitives..  Par contre, interdit d'utiliser cette technique, mis a part le prof!",
-  image_url: nil,
-  })
+# puts " Choice 2 Flemme Cours"
+# event_flemme_choice_2 = Choice.new({
+#   title:"La flemme l’emporte et tu décides de suivre nonchalamment le cours.",
+#   description:"Pari gagnant ! Toutes les infos dont tu as besoin sont dans les slides et sources du cours. Stress -, malus tu prends des mauvaises habitudes ??",
+#   score_impact:  20,
+#   stress_impact: 00 ,
+# })
+# event_flemme_choice_2.event = event_flemme
+# event_flemme_choice_2.save
 
-event_scaffold.save
+# #-------------------------------------------------------------
+# puts "Creating Scaffold event"
+# event_scaffold = Event.new({
+#   title: "Scaffold!",
+#   description: "On nous a appris le scaffold, pour eviter de faire ces methodes interminables et repetitives..  Par contre, interdit d'utiliser cette technique, mis a part le prof!",
+#   image_url: nil,
+#   })
 
-puts " Choice 1 Scaffold"
- event_scaffold_choice_1 = Choice.new({
-  title: "M'en fiche j'utilise quand meme",
-  description: "Cool, pas de stress, tu prends meme le ptit dej avec le prof",
-  score_impact: 20,
-  stress_impact: 0,
-})
+# event_scaffold.save
 
- event_scaffold_choice_1.event = event_scaffold
- event_scaffold_choice_1.save
+# puts " Choice 1 Scaffold"
+#  event_scaffold_choice_1 = Choice.new({
+#   title: "M'en fiche j'utilise quand meme",
+#   description: "Cool, pas de stress, tu prends meme le ptit dej avec le prof",
+#   score_impact: 20,
+#   stress_impact: 0,
+# })
 
-puts " Choice 2 Scaffold"
-event_scaffold_choice_2 = Choice.new({
-  title:"Je respecte l'instruction du prof",
-  description:"Ca fait rien, mais t'es un champion quand meme",
-  score_impact:  0,
-  stress_impact: 0 ,
-})
-event_scaffold_choice_2.event = event_scaffold
-event_scaffold_choice_2.save
+#  event_scaffold_choice_1.event = event_scaffold
+#  event_scaffold_choice_1.save
 
-# puts " Creating games event"
-# game_event_1 = GameEvent.new(game_id: game_1, event_id: event_yoga)
-# game_event_1.save
+# puts " Choice 2 Scaffold"
+# event_scaffold_choice_2 = Choice.new({
+#   title:"Je respecte l'instruction du prof",
+#   description:"Ca fait rien, mais t'es un champion quand meme",
+#   score_impact:  0,
+#   stress_impact: 0 ,
+# })
+# event_scaffold_choice_2.event = event_scaffold
+# event_scaffold_choice_2.save
