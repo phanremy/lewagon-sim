9128262eef5e6a88d29f5e4bc5a887c6ddcb4469
Remy Phan <phan.remy@gmail.com>
2019-12-02 10:55:46 +0100

    container change, responsive, no more bug swipe

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,25 @@ import { incrementStress } from '../plugins/init_increment';
 import { incrementScore } from '../plugins/init_increment';
 import { initSwipe } from '../plugins/init_swipe';
 
-initTyped();
-incrementStress();
-incrementScore();
-initSortable();
-initDisappearingImpact();
+if (document.querySelector("#typed")) {
+  initTyped();
+}
+
+if (document.querySelector("[data-stress-impact]")) {
+  incrementStress();
+}
+
+if (document.querySelector("[data-score-display]")) {
+  incrementScore();
+}
+
+if (document.querySelector('#sortable-img-card')) {
+  initSortable();
+}
+
+if (document.querySelectorAll('.bounceInDown')) {
+  initDisappearingImpact();
+}
+
 initSwipe();
+
