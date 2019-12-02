cf364df3d83b1878a02a44c254c99a91eb3acb83
Nicolas Pierson <nicolas.pierson57@gmail.com>
2019-12-02 14:16:17 +0100

    add swipe indicators

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,39 @@ import { incrementStress } from '../plugins/init_increment';
 import { incrementScore } from '../plugins/init_increment';
 import { initSwipe } from '../plugins/init_swipe';
 
+const arrowLeft = document.querySelector('.no-swipe-arrow-left');
+  setTimeout(function() {
+    arrowLeft.classList.toggle("no-swipe-arrow-left");
+    arrowLeft.classList.toggle("swipe-arrow");
+  }, 1000);
+  setTimeout(function() {
+    arrowLeft.classList.toggle("fadeOutLeft");
+  }, 2000);
+
+
+
+const arrowRight = document.querySelector('.no-swipe-arrow-right');
+  setTimeout(function() {
+    arrowRight.classList.toggle("no-swipe-arrow-right");
+    arrowRight.classList.toggle("swipe-arrow");
+
+  }, 1000);
+  setTimeout(function() {
+    arrowRight.classList.toggle("fadeOutRight");
+  }, 2000);
+
+
+
+
+
+
+
 initTyped();
 incrementStress();
 incrementScore();
 initSortable();
 initDisappearingImpact();
 initSwipe();
+
+
+
